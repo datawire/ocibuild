7f4f78fec78b56a710573f5f271c84d78536fc1d
Luke Shumaker <lukeshu@datawire.io>
2020-12-28 10:21:50 -0700

    add a test

---
+++ pkg/python/stat_test.go
@@ -0,0 +1,23 @@
+package python_test
+
+import (
+	"fmt"
+	"os/exec"
+	"testing"
+	"testing/quick"
+
+	"github.com/datawire/layertool/pkg/python"
+)
+
+func TestStatModeString(t *testing.T) {
+	fn := func(m python.StatMode) bool {
+		act := m.String()
+		exp, _ := exec.Command("python3", "-c",
+			fmt.Sprintf(`import stat; print(stat.filemode(%d), end="")`, m)).
+			Output()
+		return act == string(exp)
+	}
+	if err := quick.Check(fn, nil); err != nil {
+		t.Error(err)
+	}
+}
